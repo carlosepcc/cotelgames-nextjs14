1397718a471390ec82b10ecb01d544e0cbb3c146
Carlos Enrique <44654304+carlosepcc@users.noreply.github.com>
2025-03-13 14:49:44 -0400

    fix(ui): hide cell overflow

--- src/games/XOR/Cell.tsx
+++ src/games/XOR/Cell.tsx
@@ -12,7 +12,7 @@ export default function Cell ({value,symbol,color,mark = false,enabled,onClick}:
     return value != null ? (
       <button
       disabled
-        className={`font-mono p-1 pb-0 cursor-pointer size-8 md:size-12 lg:size-16 xl:size-20 flex items-center justify-center aspect-square transition-all duration-300 text-white/95 font-black sm:text-2xl lg:text-4xl xl:text-6xl hover:scale-95 shadow-inner active:scale-150 active:transition-none bg-[#ff4141d4]  round ${
+        className={`overflow-hidden font-mono p-1 pb-0 cursor-pointer size-8 md:size-12 lg:size-16 xl:size-20 flex items-center justify-center aspect-square transition-all duration-300 text-white/95 font-black sm:text-2xl lg:text-4xl xl:text-6xl hover:scale-95 shadow-inner active:scale-150 active:transition-none bg-[#ff4141d4]  round ${
           mark && "ring-2 sm:ring-4 ring-slate-700 border-2"
         } pointer-events-none border-b-2
           ${enabled && "border-2 lg:border-4"}
